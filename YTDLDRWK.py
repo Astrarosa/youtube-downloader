7d794ecbe13729bb4486ed1cfbae7fa1a381b26e
Astrarosa <catiwalaan.joel@gmail.com>
2026-05-29 23:00:32 +0800

    Update YTDLDRWK.py

--- YTDLDRWK.py
+++ YTDLDRWK.py
@@ -58,7 +58,7 @@ class YTDLPHandler:
                         'preferredcodec': 'mp3',
                         'preferredquality': '192',
                     }],
-                    'outtmpl': os.path.join(DOWNLOAD_FOLDER, '%(title)s.%(ext)s'),
+                   'outtmpl': os.path.join(target_folder, '%(title)s.%(ext)s'),
                     'progress_hooks': [self.progress_hook],
                     'quiet': True,
                     'no_warnings': True,
@@ -76,7 +76,7 @@ class YTDLPHandler:
                     'cookiefile': 'cookies.txt',
                     # ADD THIS EXTRACTOR LINE:
                     'extractor_args': {'youtube': {'player_client': ['android', 'ios']}},
-                    'outtmpl': os.path.join(DOWNLOAD_FOLDER, '%(title)s.%(ext)s'),
+                   'outtmpl': os.path.join(target_folder, '%(title)s.%(ext)s'),
                     'progress_hooks': [self.progress_hook],
                     'merge_output_format': 'mp4',
                     'quiet': True,
